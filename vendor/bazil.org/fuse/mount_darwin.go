1445c9fcb75f1efb0fb1621913d777f7657610d2
Benjamin Wester <bwester@bwester.org>
2016-05-12 20:50:58 +0000

    Update dependencies
    * bazil.org/fuse -> 5d02b067
    * github.com/Sirupsen/logrus -> v0.10.0
    * github.com/hashicorp/consul -> v0.6.3
    * golang.org/x/net -> 96dbb961
    
    New dependencies
    * github.com/hashicorp/go-cleanhttp @ ce617e79
    * github.com/hashicorp/serf/coordinate @ 39c7c062
    * golang.org/x/sys/unix @ 833a04a1

--- vendor/bazil.org/fuse/mount_darwin.go
+++ vendor/bazil.org/fuse/mount_darwin.go
@@ -14,9 +14,8 @@ import (
 )
 
 var (
-	errNoAvail         = errors.New("no available fuse devices")
-	errNotLoaded       = errors.New("osxfuse is not loaded")
-	errOSXFUSENotFound = errors.New("cannot locate OSXFUSE")
+	errNoAvail   = errors.New("no available fuse devices")
+	errNotLoaded = errors.New("osxfuse is not loaded")
 )
 
 func loadOSXFUSE(bin string) error {
@@ -91,7 +90,7 @@ func isBoringMountOSXFUSEError(err error) bool {
 	return false
 }
 
-func callMount(bin string, dir string, conf *mountConfig, f *os.File, ready chan<- struct{}, errp *error) error {
+func callMount(bin string, daemonVar string, dir string, conf *mountConfig, f *os.File, ready chan<- struct{}, errp *error) error {
 	for k, v := range conf.options {
 		if strings.Contains(k, ",") || strings.Contains(v, ",") {
 			// Silly limitation but the mount helper does not
@@ -116,12 +115,10 @@ func callMount(bin string, dir string, conf *mountConfig, f *os.File, ready chan
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "MOUNT_FUSEFS_CALL_BY_LIB=")
 
-	// TODO this is used for fs typenames etc, let app influence it
-
-	// for OSXFUSE 2.x
-	cmd.Env = append(cmd.Env, "MOUNT_FUSEFS_DAEMON_PATH="+bin)
-	// for OSXFUSE 3.x
-	cmd.Env = append(cmd.Env, "MOUNT_OSXFUSE_DAEMON_PATH="+bin)
+	daemon := os.Args[0]
+	if daemonVar != "" {
+		cmd.Env = append(cmd.Env, daemonVar+"="+daemon)
+	}
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -197,12 +194,12 @@ func mount(dir string, conf *mountConfig, ready chan<- struct{}, errp *error) (*
 		if err != nil {
 			return nil, err
 		}
-		err = callMount(loc.Mount, dir, conf, f, ready, errp)
+		err = callMount(loc.Mount, loc.DaemonVar, dir, conf, f, ready, errp)
 		if err != nil {
 			f.Close()
 			return nil, err
 		}
 		return f, nil
 	}
-	return nil, errOSXFUSENotFound
+	return nil, ErrOSXFUSENotFound
 }
